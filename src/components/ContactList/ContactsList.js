4b3a634710ebb1ece86fd9dc7eb63d1d4665bcc7
Fedir <zi-12feodor@ukr.net>
2021-02-03 09:56:57 +0200

    contactlist to table

--- src/components/ContactList/ContactsList.js
+++ src/components/ContactList/ContactsList.js
@@ -1,35 +1,147 @@
-import { useSelector, useDispatch } from 'react-redux';
-import actionsOperations from '../redux/contacts/contacts-operations';
-import s from './Contacts.module.css';
-import * as contactsSelectors from '../redux/contacts/contacts-selectors';
+import { useSelector, useDispatch } from "react-redux";
+import actionsOperations from "../redux/contacts/contacts-operations";
+import s from "./Contacts.module.css";
+import * as contactsSelectors from "../redux/contacts/contacts-selectors";
+import { useState } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+
+const columns = [
+  { id: "name", label: "Name", minWidth: 100 },
+  { id: "number", label: "Number", minWidth: 200 },
+  {
+    id: "btn",
+    label: "Reg",
+    minWidth: 70,
+    align: "right",
+  },
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+// function StickyHeadTable() {
+//   const classes = useStyles();
+//   const [page, setPage] = React.useState(0);
+//   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+//   const handleChangePage = (event, newPage) => {
+//     setPage(newPage);
+//   };
+
+//   const handleChangeRowsPerPage = (event) => {
+//     setRowsPerPage(+event.target.value);
+//     setPage(0);
+//   };
+// }
 
 function Contacts() {
   const dispatch = useDispatch();
   const contacts = useSelector(contactsSelectors.getVisibleContacts);
 
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
   return (
-    <ul className={s.contactList}>
-      {contacts &&
-        contacts.map(c => {
-          return (
-            <li className={s.item} key={c.id}>
-              <p className={s.contactItem}>
-                <span>
-                  <strong>{c.name}:</strong> {c.number}
-                </span>
-                <button
-                  className="btn"
-                  onClick={() =>
-                    dispatch(actionsOperations.deleteContacts(c.id))
-                  }
+    // <ul className={s.contactList}>
+    //   {contacts &&
+    //     contacts.map(c => {
+    //       return (
+    //         <li className={s.item} key={c.id}>
+    //           <p className={s.contactItem}>
+    //             <span>
+    //               <strong>{c.name}:</strong> {c.number}
+    //             </span>
+    //             <button
+    //               className="btn"
+    //               onClick={() =>
+    //                 dispatch(actionsOperations.deleteContacts(c.id))
+    //               }
+    //             >
+    //               Delete
+    //             </button>
+    //           </p>
+    //         </li>
+    //       );
+    //     })}
+    // </ul>
+
+    //------
+
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
                 >
-                  Delete
-                </button>
-              </p>
-            </li>
-          );
-        })}
-    </ul>
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {contacts
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={contacts.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+
+    //--------------
   );
 }
 
